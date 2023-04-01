01a73b848831cc6797375a50ef830219813d26a0
Albany Pineda <agpv.dev@gmail.com>
2023-04-01 17:13:50 -0500

    Build folder removed

--- build/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PAGE_SIZE = void 0;
-exports.PAGE_SIZE = 5;
-//# sourceMappingURL=constants.js.map
--- build/modules/users/dtos/user-patch-dto.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=user-patch-dto.js.map
