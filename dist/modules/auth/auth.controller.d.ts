d452dfe9b8c80c32b34d04ebdabb70157d2cb497
Albany Pineda <agpv.dev@gmail.com>
2023-04-01 17:22:54 -0500

    Dist folder removed

--- dist/modules/auth/auth.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { AuthService } from './auth.service';
-import { SignUpDto } from './dtos/sign-up.dto';
-export declare class AuthController {
-    private authService;
-    constructor(authService: AuthService);
-    findAll(): string;
-    signUp(signUpDto: SignUpDto): Promise<SignUpDto>;
-}
--- dist/modules/auth/auth.service.d.ts
+++
@@ -1,5 +0,0 @@
-import { SignUpDto } from './dtos/sign-up.dto';
-export declare class AuthService {
-    findAll(): string;
-    signUp(signUpDto: SignUpDto): SignUpDto;
-}
